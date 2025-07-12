30122bcf1a31559d4eb80b5aa3877d1ee9fee1c9
way <3117635571@qq.com>
2025-07-12 20:45:34 +0800

    Site updated: 2025-07-12 20:45:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/09/hello-world/","2025/07/10/MyBlog/","2025/07/09/我的第一篇文章/"];function toRandomPost(){
+var posts=["2025/07/10/MyBlog/","2025/07/09/hello-world/","2025/07/12/配置Anaconda/","2025/07/09/我的第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
